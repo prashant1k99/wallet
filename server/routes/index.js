ad2492df2927d83334b706297af04f0d57551d03
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-08-07 17:56:23 +0530

    Imported Route and Tested

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 
+const setupRoute = require('./setup')
+const transactRoute = require('./transact')
+const transactionRoute = require('./transaction')
+const walletRoute = require('./wallet')
+
 const app = express()
 app.use(express.json())
 
-app.use('/path', 'fileName')
+app.use('/setup', setupRoute)
+app.use('/transact', transactRoute)
+app.use('/transactions', transactionRoute)
+app.use('/wallet', walletRoute)
 
 module.exports = app
