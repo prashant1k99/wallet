3fec469d012d63c7007cb0af2be5e483970a025e
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-08-07 17:55:57 +0530

    Completed Transaction Route

---
+++ server/routes/transaction.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const { Transaction } = require('../models')
+
+const router = express.Router()
+
+router.get('/:walletId', async (req, res) => {
+  try {
+    const walletId = req.params.walletId,
+      skip = req.query.skip || 0,
+      limit = req.query.limit || 20
+    const allTransactions = await Transaction.find({
+      walletId
+    }).skip(skip * limit).limit(limit)
+    const result = allTransactions.map((record) => {
+      return {
+        id: record._id.toString(),
+        walletId: record._doc.walletId.toString(),
+        amount: parseFloat(record._doc.amount.toString()),
+        balance: parseFloat(record._doc.balance.toString()),
+        description: record._doc.description,
+        date: record._doc.createdAt,
+        type: record._doc.type
+      }
+    })
+    res.status(200).send(result)
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('Something Went Wrong')
+  }
+})
+
+module.exports = router
