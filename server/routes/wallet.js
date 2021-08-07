72a6ae694ac361c6035a928c58e512fc4ffab4b7
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-08-07 17:56:08 +0530

    completed Wallet Route

---
+++ server/routes/wallet.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { Wallet } = require('../models')
+
+const router = express.Router()
+
+router.get('/:walletId', async (req, res) => {
+  try {
+    const wallet = await Wallet.findById(req.params.walletId)
+    if (!wallet) {
+      const error = new Error('No wallet found with the Specified Id')
+      error.code = 400
+      throw new Error
+    }
+    res.status(200).send({
+      id: wallet._id.toString(),
+      balance: parseFloat(wallet._doc.balance.toString()),
+      name: wallet._doc.name,
+      date: wallet._doc._createdAt
+    })
+  } catch(err) {
+    if ((err.code / 100) % 4 === 0) 
+      res.status(err.code).send(err.message)
+    else {
+      console.log(err)
+      res.status(500).send('Something Went Wrong')
+    }
+  }
+})
+
+module.exports = router
