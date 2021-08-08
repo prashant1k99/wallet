63b31ef1d477fbd4a6eef882664ecd1110813678
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-08-08 13:52:02 +0530

    Added CSV Transaction Download Route

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,7 +1,40 @@
 const express = require('express')
 const { Transaction } = require('../models')
+const converter = require('json-2-csv')
 
 const router = express.Router()
+
+router.get('/download/:walletId', async (req, res) => {
+  try {
+    const walletId = req.params.walletId,
+      skip = parseInt(req.query.skip),
+      limit = parseInt(req.query.limit),
+      order = req.query.order === 'desc' ? req.query.order : 'asc' 
+    const allTransactions = await Transaction.find({
+      walletId
+    }).limit(limit).skip(skip).sort({
+      createdAt: order
+    })
+    const result = allTransactions.map((record) => {
+      return {
+        id: record._id.toString(),
+        walletId: record._doc.walletId.toString(),
+        amount: record._doc.amount,
+        balance: record._doc.balance,
+        description: record._doc.description,
+        date: record._doc.createdAt,
+        type: record._doc.type
+      }
+    })
+    const csv = await converter.json2csvAsync(result)
+    res.setHeader('Content-disposition', `attachment; filename=transaction-report-${walletId}.csv`)
+    res.set('Content-Type', 'text/csv')
+    return res.status(200).send(csv)
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('Something Went Wrong')
+  }
+})
 
 router.get('/:walletId', async (req, res) => {
   try {
